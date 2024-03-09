57bbf87b64c85d7d8366c4a93cb65b1a7ebe1feb
Amirali Monjar <amirali.mnj@gmail.com>
2024-03-09 11:48:08 +0000

    add character movement

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        public Transform targetFollow;
+        public bool followVertical = false;
+        public bool followHorizontal = true;
+        public float smoothTime;
+        private void FixedUpdate()
+        {
+            var currentPos = transform.position;
+            var targetPos = new Vector3(targetFollow.position.x, targetFollow.position.y, currentPos.z);
+            if (!followVertical)
+                targetPos.y = currentPos.y;
+            if (!followHorizontal)
+                targetPos.x = currentPos.x;
+
+            transform.position = Vector3.Lerp(currentPos, targetPos, Time.fixedDeltaTime * smoothTime);
+        }
+    }
+}
--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -1,18 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float topSpeed = 4;
+    public float accelerationMag = 4f;
+    public float stopAccelerationMag = 40f;
+    public Rigidbody2D rBody;
+    public Animator animator;
+
+    private Vector2 _currentMovement;
+
+
+    private void Update()
     {
-        
+        _currentMovement.x = Input.GetAxisRaw("Horizontal");
+        _currentMovement.y = Input.GetAxisRaw("Vertical");
+        _currentMovement.Normalize();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void ApplyAcceleration(Vector2 acc)
     {
-        
+        var acceleratedSpeed = rBody.velocity + acc;
+        rBody.velocity = acceleratedSpeed.magnitude > topSpeed
+            ? acceleratedSpeed.normalized * topSpeed
+            : acceleratedSpeed;
+    }
+    public void StopInY()
+    {
+        var acc = (-rBody.velocity.y) * (Time.fixedDeltaTime * stopAccelerationMag);
+        ApplyAcceleration(new Vector2(0, acc));
+    }
+    public void StopInX()
+    {
+        var acc = (-rBody.velocity.x) * (Time.fixedDeltaTime * stopAccelerationMag);
+        ApplyAcceleration(new Vector2(acc, 0));
+    }
+
+    public void Move()
+    {
+        var isChangingDir = Vector2.Dot(_currentMovement, rBody.velocity) < 0;
+
+        var acc = _currentMovement * (Time.fixedDeltaTime *
+                                      (isChangingDir ? (accelerationMag + stopAccelerationMag) : accelerationMag));
+        ApplyAcceleration(acc);
+    }
+
+    private void FixedUpdate()
+    {
+        var isStoppingInX = (Mathf.Abs(_currentMovement.x) < 0.01f);
+        var isStoppingInY = (Mathf.Abs(_currentMovement.y) < 0.01f);
+
+        if (isStoppingInX)
+        {
+            StopInX();
+        }
+        if (isStoppingInY)
+        {
+            StopInY();
+        }
+        if(!isStoppingInY || !isStoppingInX)
+        {
+            Move();
+        }
     }
 }
